e3025f8be6b76bfcbb7acfc325cf1d3cdad45633
Sergio Vargas <svargas.ext.uber@gmail.com>
2024-10-29 19:55:34 -0500

    read code completed

--- admin/index.php
+++ admin/index.php
@@ -2,14 +2,31 @@
 
     session_start();
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+
+    require '../includes/config/database.php';
+
      $header_text = 'Administrador de reseñas';
      require '../includes/functions.php';
      includeTemplate("header", $header_text);
+
+     $db = connectDB();
+
+     $query = "SELECT * FROM reviews";
+
+     $result = mysqli_query($db, $query);
+
+     echo '<pre>'; 
+     var_dump($result->fetch_assoc());
+     echo '</pre>';
 ?>
     
     <main class="container">
     <?php 
-        if ($_SESSION['errors']) {
+        if (isset($_SESSION['errors'])) {
     ?>
         <div class="alert success">
             <?php echo $_SESSION['errors']; ?>
@@ -18,6 +35,41 @@
         }
     ?>
         <a href="/blog_sobreliteratura/admin/reviews/create.php" class="button-inline">Nueva reseña</a>
+
+        <table class="reviews">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Titulo</th>
+                    <th>Autor</th>
+                    <th>Imagen</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            
+            <tbody>
+            <?php 
+            
+                if ($result->num_rows > 0) {
+                
+                while ($row = mysqli_fetch_assoc($result)) : ?>
+                       
+                    <tr>
+                        <td><?php echo $row['id']; ?></td>
+                        <td><?php echo $row['title']; ?></td>
+                        <td><?php echo $row['author']; ?></td>
+                        <td><img class="image_review" src="<?php echo "../uploads/"  . $row['image']; ?>"></td>
+                        <td>
+                            <button class="button-block">Eliminar</button>
+                            <button class="button-block">Editar</button>
+                        </td>
+                    </tr>
+                </tbody>
+                <?php endwhile; } ?>
+            
+        </table>
+
+        
     </main>
 
 <?php 
